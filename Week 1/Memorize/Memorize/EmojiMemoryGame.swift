51f55f62434bdee2b50840eefd241cda9f46b347
alvarovs89 <67175098+alvarovs89@users.noreply.github.com>
2022-01-02 11:15:55 +0800

    all done until class 3

--- Week 1/Memorize/Memorize/EmojiMemoryGame.swift
+++ Week 1/Memorize/Memorize/EmojiMemoryGame.swift
@@ -3,6 +3,45 @@
 //  Memorize
 //
 //  Created by Alvaro Valdes Salazar on 29-12-21.
-//
 
-import Foundation
+    // THIS IS THE VIEW-MODEL
+
+import SwiftUI
+
+
+//func makeCardContent(index: Int)-> String {
+//    return "😀"
+//
+//}
+
+class EmojiMemoryGame {
+    
+    static let emojis = ["✈️","🚗","🚓","🛵","🚒","🚚","🛺","🚝","🚀","🚁","🚕","🚎","🛴","🚲","🚈","🛶","🛸","🚂","🚜","🚙","🚌","🏎","🚢","🛰"]
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+            EmojiMemoryGame.emojis[pairIndex]
+        }
+    }
+    
+    
+        // private makes a class private, so it is accesible only by the class, however private(set) (set) will allow others struct or classes to view the class but not change it
+    
+    
+        //   private(set) var model: MemoryGame<String>
+    
+    
+        //    private var model: MemoryGame<String> =
+        //    MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+        //        EmojiMemoryGame.emojis[pairIndex]
+        //    }
+    
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+            // this is read only
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+        
+    }
+    
+}
---
+++ Week 1/Memorize/Memorize/MemoryGame.swift
@@ -0,0 +1,35 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Alvaro Valdes Salazar on 28-12-21.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards:Array<Card>
+    
+    func choose(_ card: Card){
+        
+    }
+    
+    init(numberOfPairsOfCards: Int, createCardContent: (Int)-> CardContent) {
+        cards = Array<Card>()
+            // add numberOfPairsOfCards x 2 cards to cards array
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = createCardContent(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+
+        }
+        
+    }
+    
+    
+    struct Card {
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
+        var content: CardContent
+    }
+}
